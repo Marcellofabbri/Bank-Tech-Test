be23582ffdf4b8b64b1880260d9ff340b1cb21b2
Marcellofabbri <marcellogfabbri@gmail.com>
2020-02-24 12:04:50 +0000

    add deposit method to put money onto account

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -6,4 +6,12 @@ describe Account do
       expect(subject.balance).to eq 0
     end
   end
+
+  describe '#deposit' do
+    it 'lets a user add money to their account' do
+      subject.deposit(20)
+      expect(subject.balance).to eq 20
+    end
+  end
 end
+
