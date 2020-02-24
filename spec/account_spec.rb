792d548f1df9c407e67ef6ce4b7a7b464f52e9c4
Marcellofabbri <marcellogfabbri@gmail.com>
2020-02-24 12:00:13 +0000

    add initialize method with a balance of 0

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,5 +1,9 @@
 require 'account'
 
 describe Account do
-  
+  describe '#initialize' do
+    it 'is initialized with a balance field set to zero upon instantiation' do
+      expect(subject.balance).to eq 0
+    end
+  end
 end
