5ce3c715a94e709a3f34a4a647e941b12dc9356a
Marcellofabbri <marcellogfabbri@gmail.com>
2020-02-25 15:27:07 +0000

    change to rubocop_spec to satisfy travis condition

--- spec/linters/rubocop_spec.rb
+++ spec/linters/rubocop_spec.rb
@@ -1,9 +1,10 @@
-require 'spec_helper'
+require_relative '../spec_helper'
 
 RSpec.describe 'rubocop analysis' do
   subject(:report) { `rubocop` }
 
   it 'has no offenses' do
-    expect(report).to match(/no\ offenses\ detected/)
+    expect(report).to match('/no\ offenses\ detected/')
+    p report
   end
 end
