dcef1bf1e6ccf164d7ce602d0b6ac4e900c23fa8
Marcellofabbri <marcellogfabbri@gmail.com>
2020-02-25 12:17:11 +0000

    extract statement_header method and move to instante variable

--- lib/printer.rb
+++ lib/printer.rb
@@ -5,18 +5,15 @@ class Printer
   attr_reader :account
   def initialize(account = Account.new)
     @account = account
+    @statement_header = 'date || credit || debit || balance'
   end
 
   def print_statement
-    puts statement_header
+    puts @statement_header
     puts records_retriever
   end
 
   private
-
-  def statement_header
-    'date || credit || debit || balance'
-  end
 
   def records_retriever
     account.transactions.each { |record| record.render }
